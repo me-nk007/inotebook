748bd79213a9e040142a3ee19a6e8c1e68e46648
me-nk007 <nishantkarn007@gmail.com>
2023-07-31 20:26:22 +0545

    Adding Noteitem and Notes as a separate component.

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,7 +13,63 @@ const NoteState = (props)=>{
           "tag": "personal",
           "date": "2023-07-30T12:03:09.258Z",
           "__v": 0
-        }
+        },
+        {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          {
+            "_id": "64c7c6dfba78a66aefbd0db9",
+            "user": "64c549358ac9c02d91474618",
+            "title": "Second Note",
+            "description": "Please access the datbase",
+            "tag": "mongodb",
+            "date": "2023-07-31T14:36:15.864Z",
+            "__v": 0
+          },
+          
+        
       ]
       const [notes, setNotes] = useState(notesInitial);
    
