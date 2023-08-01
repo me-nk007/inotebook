d7f52fa2b3e5ad22c18fe200fd8d21933b7f27b8
me-nk007 <nishantkarn007@gmail.com>
2023-08-01 13:23:03 +0545

    Added Fetch Notes Functionality

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import noteContext from "../context/notes/noteContext"
 
 const Noteitem = (props) => {
+const context = useContext(noteContext);
+
+
+   const {deleteNote} = context;
    const {note} = props;
    const myStyle = {
     color: '#ed0202'
@@ -15,7 +20,7 @@ const Noteitem = (props) => {
             <h5 className="card-title">{note.title}</h5>
             <p className="card-text my-3">{note.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolorum quidem, amet reprehenderit dolores non est ab alias, harum, aspernatur vitae! Aliquid incidunt eum amet ab provident esse repudiandae, voluptate perspiciatis quo enim necessitatibus, possimus, facilis quam magnam nisi voluptas?</p>
             <div className="d-flex justify-content-between my-3">
-            <i className="fa-solid fa-trash fa-xl mx-2" style={myStyle}></i>
+            <i className="fa-solid fa-trash fa-xl mx-2" style={myStyle} onClick={()=>{deleteNote(note._id)}} ></i>
             <i className="fa-solid fa-pen-to-square fa-xl mx-2" style={yourStyle}></i>
             </div>
            
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -7,14 +7,14 @@ import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, addNote} = context;
+    const {notes} = context;
       return (
     <>
     <AddNote/>
     <div className="row my-3">
       <h2>Your Notes</h2>
      {notes.map((note)=>{
-        return <Noteitem key={note.id} note={note}/>
+        return <Noteitem key={note._id} note={note}/>
       })}
     </div>
     </>
